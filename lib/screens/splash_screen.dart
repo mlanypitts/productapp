9018fe1379766e2972a19439f8a6a6d39a9a025b
zaynab.abdoolrahman <zaynab.abdoolrahman@vinci-energies.com>
2023-05-11 16:21:21 +0400

    feat: initial project added.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:product_app/utils/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 8), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const NextScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.antiFlashWhite,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              child: Lottie.asset(
+                'assets/lottie/splash.json',
+                width: 200,
+                height: 200,
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text('Your products at your doorstep',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Next Screen')),
+      body: const Center(child: Text('Welcome to the next screen!')),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color antiFlashWhite = Color(0xfff6f1f1);
+  static const Color lightBlue = Color(0xffAFD3E2);
+  static const Color pacificBlue = Color(0xff19A7CE);
+  static const Color seaBlue = Color(0xff146C94);
+  static const Color transparent = Colors.transparent;
+}
